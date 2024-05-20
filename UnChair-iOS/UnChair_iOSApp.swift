7cb6ebf7f8ece7e18f3b0af7c217b64c78c4105b
mehadishakil <mehadishakil469@gmail.com>
2024-05-20 15:47:19 +0600

    Initial Commit

---
+++ UnChair-iOS/UnChair_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnChair_iOSApp.swift
+//  UnChair-iOS
+//
+//  Created by Mehadi Hasan on 20/5/24.
+//
+
+import SwiftUI
+
+@main
+struct UnChair_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
