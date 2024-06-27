9f3edc94ec3002799fb5e1819f983ecb924bd0d7
mehadishakil <mehadishakil469@gmail.com>
2024-06-27 11:59:10 +0600

    horizontal calendarview added

---
+++ UnChair-iOS/Core/Home/Views/HCalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  HCalendarView.swift
+//  UnChair-iOS
+//
+//  Created by Mehadi Hasan on 27/6/24.
+//
+
+import SwiftUI
+
+struct HCalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HCalendarView()
+}
