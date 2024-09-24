3c9d95670a3e4c325b0fa3be4e2409afcedc534b
mehadishakil <mehadishakil469@gmail.com>
2024-09-25 02:56:02 +0600

    Redesign Edit Profile Screen

--- UnChair-iOS/Core/Profile/Views/EditProfile.swift
+++ UnChair-iOS/Core/Profile/Views/EditProfile.swift
@@ -5,222 +5,123 @@
 //  Created by Mehadi Hasan on 2/7/24.
 //
 
+
 import SwiftUI
 import PhotosUI
 
 struct EditProfile: View {
-    
-    @State private var avatarImage : UIImage?
-    @State private var photosPickerItem : PhotosPickerItem?
-    @State private var name : String = "James Bond"
-    @State private var email : String = "jamesbond007@gmail.com"
-    @State private var oldPassword : String = ""
-    @State private var newPassword : String = ""
-    @State private var isOldPasswordVisible: Bool = false
-    @State private var isNewPasswordVisible: Bool = false
-    
-    
+    @State private var first_name: String = "John"
+    @State private var last_name: String = "Smith"
+    @State private var email: String = "Johnsmithmobbin@gmail.com"
+    @State private var avatarImage: UIImage?
+    @State private var photosPickerItem: PhotosPickerItem?
+
     var body: some View {
-        
-        VStack {
-            VStack {
-                VStack{
-                    PhotosPicker(selection: $photosPickerItem, matching: .images) {
+        NavigationView {
+            ZStack {
+                Color.black.edgesIgnoringSafeArea(.all)
+                
+                VStack(spacing: 20) {
+                    ZStack(alignment: .bottomTrailing) {
                         if let avatarImage = avatarImage {
                             Image(uiImage: avatarImage)
                                 .resizable()
                                 .aspectRatio(contentMode: .fill)
-                                .frame(width: 130, height: 130)
+                                .frame(width: 120, height: 120)
                                 .clipShape(Circle())
                         } else {
                             Image(systemName: "person.circle.fill")
                                 .resizable()
                                 .aspectRatio(contentMode: .fill)
-                                .frame(width: 130, height: 130)
+                                .frame(width: 120, height: 120)
                                 .clipShape(Circle())
                                 .foregroundColor(.gray)
                         }
-                    }
-                    
-                }
-                .padding(10)
-                .onChange(of: photosPickerItem) { _, _ in
-                    Task{
-                        if let photosPickerItem,
-                           let data = try? await photosPickerItem.loadTransferable(type: Data.self){
-                            if let image = UIImage(data: data){
-                                avatarImage = image
+                        
+                        PhotosPicker(selection: $photosPickerItem, matching: .images) {
+                            Image(systemName: "pencil")
+                                .foregroundColor(.white)
+                                .frame(width: 30, height: 30)
+                                .background(Color.blue)
+                                .clipShape(Circle())
+                        }
+                        .onChange(of: photosPickerItem) { newValue in
+                            Task {
+                                if let photosPickerItem = photosPickerItem,
+                                   let data = try? await photosPickerItem.loadTransferable(type: Data.self),
+                                   let image = UIImage(data: data) {
+                                    avatarImage = image
+                                }
                             }
                         }
-                        photosPickerItem = nil
+                    }
+                    .padding(.bottom, 20)
+
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("First Name")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                            .fontWeight(.bold)
+                        TextField("", text: $first_name)
+                            .padding(.leading)
+                            .frame(height: 50)
+                            .background(Color(.darkGray))
+                            .cornerRadius(8)
+                            .foregroundColor(.white)
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Last Name")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                            .fontWeight(.bold)
+                        TextField("", text: $last_name)
+                            .padding(.leading)
+                            .frame(height: 50)
+                            .background(Color(.darkGray))
+                            .cornerRadius(8)
+                            .foregroundColor(.white)
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Email")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                            .fontWeight(.bold)
+                        TextField("", text: $email)
+                            .padding(.leading)
+                            .frame(height: 50)
+                            .background(Color(.darkGray))
+                            .cornerRadius(8)
+                            .foregroundColor(.gray)
+                            .disabled(true)
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        // Save action
+                    }) {
+                        Text("Save")
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(8)
+                            .font(.title3)
+                            .fontWeight(.semibold)
                     }
                 }
-                Text("Profile Picture")
-                    .foregroundColor(.gray)
-                    .font(.subheadline)
-                
-                
-                HStack {
-                    Text("Name")
-                        .foregroundColor(.gray)
-                        .font(.caption)
-                    Spacer()
-                }.padding(.top)
-                TextField("Name", text: $name)
-                    .padding()
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(8)
-                    .fontWeight(.light)
-                    .overlay(
-                        HStack {
-                            Spacer()
-                            Image(systemName: "person.circle")
-                                .foregroundColor(.gray)
-                                .padding(.trailing)
-                        }
-                    )
-                
-                
-                
-                
-                HStack {
-                    Text("Email")
-                        .foregroundColor(.gray)
-                        .font(.caption)
-                    Spacer()
-                }.padding(.top)
-                TextField("Email", text: $email)
-                    .padding()
-                    .background(Color.gray.opacity(0.2))
-                    .foregroundColor(.gray)
-                    .cornerRadius(8)
-                    .disabled(true)
-                    .fontWeight(.medium)
-                    .overlay(
-                        HStack {
-                            Spacer()
-                            Image(systemName: "envelope")
-                                .foregroundColor(.gray)
-                                .padding(.trailing)
-                        }
-                    )
-                       
-    
-                
-                HStack {
-                    Text("Update Password")
-                        .foregroundColor(.gray)
-                        .font(.caption)
-                    Spacer()
-                }.padding(.top)
-                
-                
-                HStack {
-                    if isOldPasswordVisible {
-                        TextField("Enter old password", text: $oldPassword)
-                            .padding()
-                            .background(Color.gray.opacity(0.2))
-                            .cornerRadius(8)
-                            .overlay(
-                                HStack {
-                                    Spacer()
-                                    Button(action: {
-                                        isOldPasswordVisible.toggle()
-                                    }) {
-                                        Image(systemName: isOldPasswordVisible ? "eye" : "eye.slash")
-                                            .foregroundColor(.gray)
-                                            .padding(.trailing)
-                                    }
-                                }
-                            )
-                        } else {
-                            SecureField("Enter old password", text: $oldPassword)
-                                .padding()
-                                .background(Color.gray.opacity(0.2))
-                                .cornerRadius(8)
-                                .overlay(
-                                    HStack {
-                                        Spacer()
-                                        Button(action: {
-                                            isOldPasswordVisible.toggle()
-                                        }) {
-                                            Image(systemName: isOldPasswordVisible ? "eye" : "eye.slash")
-                                                .foregroundColor(.gray)
-                                                .padding(.trailing)
-                                        }
-                                    }
-                                )
-                        }
-                }
-                    
-                
-                HStack {
-                    if isNewPasswordVisible {
-                        TextField("Enter new password", text: $newPassword)
-                            .padding()
-                            .background(Color.gray.opacity(0.2))
-                            .cornerRadius(8)
-                            .overlay(
-                                HStack {
-                                    Spacer()
-                                    Button(action: {
-                                        isNewPasswordVisible.toggle()
-                                    }) {
-                                        Image(systemName: isNewPasswordVisible ? "eye" : "eye.slash")
-                                            .foregroundColor(.gray)
-                                            .padding(.trailing)
-                                    }
-                                }
-                            )
-                        } else {
-                            SecureField("Enter new password", text: $newPassword)
-                                .padding()
-                                .background(Color.gray.opacity(0.2))
-                                .cornerRadius(8)
-                                .overlay(
-                                    HStack {
-                                        Spacer()
-                                        Button(action: {
-                                            isNewPasswordVisible.toggle()
-                                        }) {
-                                            Image(systemName: isNewPasswordVisible ? "eye" : "eye.slash")
-                                                .foregroundColor(.gray)
-                                                .padding(.trailing)
-                                        }
-                                    }
-                                )
-                        }
-                }.padding(.top, 15)
-                    
-                
-                
-                
-                
-                
-                Spacer()
-                
-                Button(action: {
-                    // Save action
-                }) {
-                    Text("Save")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.black)
-                        .cornerRadius(8)
-                }
-                .padding(.top)
+                .padding()
             }
-            .padding()
-            
-            Spacer()
+            .navigationBarTitle("Profile Info", displayMode: .inline)
         }
-        .navigationTitle("Edit Profile")
-  
+        .accentColor(.white)
     }
 }
 
-#Preview {
-    EditProfile()
+struct ProfileInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfile()
+    }
 }
